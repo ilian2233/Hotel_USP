bb37dbfdf2fb208824f65a4cdab2bc99ce51292f
Vasil Kolev <vasil_kolev99@abv.bg>
2021-05-11 00:02:03 +0300

    Add InitMigration :arrow_up:

---
+++ Backend/Hotel.USP/Persistance/Migrations/20210510204656_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistance.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IsBusy = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
---
+++ Backend/Hotel.USP/Persistance/Seed.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (!context.Rooms.Any())
+            {
+                var rooms = new List<Room>
+                {
+                    new Room
+                    {
+                        IsBusy = true,
+                    },
+                    new Room
+                    {
+                        IsBusy = true,
+                    },
+                    new Room
+                    {
+                        IsBusy = true,
+                    }
+                };
+
+                await context.Rooms.AddRangeAsync(rooms);
+            }
+          
+            await context.SaveChangesAsync();
+
+        }
+    }
+}
