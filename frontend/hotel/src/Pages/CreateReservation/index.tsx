29005a03c142dbb13992f0526b605285a7333696
Iliyan <ilianbb4@gmail.com>
2021-05-16 00:16:16 +0300

    Added prettier config; Connected eslint and prettier;

--- frontend/hotel/src/Pages/CreateReservation/index.tsx
+++ frontend/hotel/src/Pages/CreateReservation/index.tsx
@@ -1,34 +1,51 @@
 import React, { useState } from "react";
 import { Button, Grid } from "@material-ui/core";
 import Calendar from "react-calendar";
-import RoomSelector from "../../Components/RoomSelector";
+import axios from "axios";
 
 const CreateReservation = (): JSX.Element => {
 	const [reservationRoom, setReservationRoom] = useState(0);
 	const [reservationPeriod, setReservationPeriod] =
 		useState<Date[] | undefined>(undefined);
 
+	const postDate = () => {
+		reservationPeriod
+			? axios({
+					method: "post",
+					url: "/user/12345",
+					data: {
+						startDate: reservationPeriod[0],
+						endDate: reservationPeriod[1],
+					},
+			  })
+			: alert("Reservation dates seems to be empty.");
+	};
+
 	return (
 		<Grid
 			container
 			component="form"
+			direction="column"
+			justify="center"
+			alignItems="center"
+			spacing={5}
 			onSubmit={() => {
 				alert(reservationRoom + "\n" + reservationPeriod);
-			}}
-		>
-			<Grid item xs={12}>
-				<RoomSelector period={[new Date(), new Date()]} />
-			</Grid>
+				postDate();
+			}}>
 			<Grid item xs={12}>
 				<Calendar
 					selectRange
+					returnValue="range"
 					minDate={new Date()}
 					onChange={(val) =>
-						setReservationPeriod(Array.isArray(val) ? val : [val])
+						setReservationPeriod(
+							Array.isArray(val) ? val : undefined
+						)
 					}
 				/>
 			</Grid>
-			<Grid item xs={12}>
+			<Grid item xs={6}>
 				<Button type="submit">Submit</Button>
 			</Grid>
 		</Grid>
