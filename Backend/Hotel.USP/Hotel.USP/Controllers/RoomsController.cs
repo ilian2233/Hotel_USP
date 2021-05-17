79da8630f74a5b8de86941f719d857720b7d402d
Vasil Kolev <vasil_kolev99@abv.bg>
2021-05-18 00:38:51 +0300

    Add Room controller - List

---
+++ Backend/Hotel.USP/Hotel.USP/Controllers/RoomsController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using System.Threading;
+using Application.Rooms;
+
+namespace Hotel.USP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public RoomsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Room>>> List(CancellationToken cancellationToken)
+        {
+            return await _mediator.Send(new List.Query(), cancellationToken);
+        }
+    }
+}
