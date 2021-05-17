64815f99ca3a37e795e6b6b5dd8de9a40a3161c5
Vasil Kolev <vasil_kolev99@abv.bg>
2021-05-18 00:38:27 +0300

    Add List Rooms in Application

---
+++ Backend/Hotel.USP/Application/Rooms/List.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Rooms
+{
+    public class List
+    {
+        public class Query : IRequest<List<Room>> { }
+
+        public class Handler : IRequestHandler<Query, List<Room>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Room>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var rooms = await _context.Rooms.ToListAsync(cancellationToken);
+                return rooms;
+            }
+        }
+
+    }
+
+}
