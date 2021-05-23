59b006042e8803eb4b0000154df40f45db548a20
Vasil Kolev <vasil_kolev99@abv.bg>
2021-05-24 01:19:50 +0300

    Seed data for reservasion  :seedling:

--- Backend/Hotel.USP/Persistance/Seed.cs
+++ Backend/Hotel.USP/Persistance/Seed.cs
@@ -11,9 +11,7 @@ namespace Persistance
     {
         public static async Task SeedData(DataContext context)
         {
-            if (!context.Rooms.Any())
-            {
-                var rooms = new List<Room>
+            List<Room> rooms = new List<Room>
                 {
                     new Room
                     {
@@ -28,10 +26,64 @@ namespace Persistance
                         IsBusy = true,
                     }
                 };
-
+            if (!context.Rooms.Any())
+            {
                 await context.Rooms.AddRangeAsync(rooms);
             }
-          
+
+            List<Customer> customers = new List<Customer>
+                {
+                    new Customer
+                    {
+                        FirstName = "Paphnoutios",
+                        LastName="Gosia"
+                    },
+                    new Customer
+                    {
+                        FirstName = "Jory",
+                        LastName="Sophia"
+                    },
+                    new Customer
+                    {
+                        FirstName = "Cason",
+                        LastName="Narayana"
+                    }
+                };
+            if (!context.Customers.Any())
+            {
+                await context.Customers.AddRangeAsync(customers);
+            }
+
+
+            if (!context.Reservations.Any())
+            {
+                var reservations = new List<Reservation>
+                {
+                    new Reservation
+                    {
+                        Customer = customers.ElementAt(0),
+                        Room = rooms.ElementAt(0),
+                        StartDate = DateTime.Now,
+                        EndDate = DateTime.Now.AddDays(14)
+                    },
+                    new Reservation
+                    {
+                        Customer = customers.ElementAt(1),
+                        Room = rooms.ElementAt(1),
+                        StartDate = DateTime.Now,
+                        EndDate = DateTime.Now.AddDays(3)
+                    },
+                    new Reservation
+                    {
+                        Customer = customers.ElementAt(2),
+                        Room = rooms.ElementAt(2),
+                        StartDate = DateTime.Now,
+                        EndDate = DateTime.Now.AddDays(4)
+                    },
+                };
+
+                await context.Reservations.AddRangeAsync(reservations);
+            }
             await context.SaveChangesAsync();
 
         }
